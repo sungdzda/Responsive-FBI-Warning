7fd412fbb9b473ebe72a02d5642df40d63d4f045
Sung Dz-da <sungdzda@vt.edu>
2019-01-25 04:26:41 +0800

    Adds content

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Responsive-FBI-Warning
 A responsive FBI warning for the terminal
+# Usage
+Configure your terminal startup file such that fbi_warning.sh is run at every login.
+For example, add the following lines to `~/.profile` :
+
+`source fbi_warning.sh`
+
+The script will manipulate the terminal using ANSI commands to draw a responsive motd regardless of the instantaneous size of your terminal. Note that your terminal width should be adequate enough to accommodate the text lines; however, any contents and the related typesetting can be freely altered via source code.
+# Preview
+The following preview shows the output across multiple terminal sizes.
+
+W=65:
+![](Images/1.png)
+
+W=80:
+![](Images/2.png)
+
+W=104:
+![](Images/3.png)
+
+***
+Note that the script will draw over your terminal; however, any content on the previous screen will be preserved in the scroll buffer. This is a nice feature by design. See the screenshot below.
+
+![](Images/4.png)
---
+++ fbi_warning.sh
@@ -0,0 +1,36 @@
+#FBI Warning by Sung Dz-da
+terminal_width=$(tput cols)
+terminal_height=$(tput lines)
+
+function put_line
+{
+	size=${#1}
+	let provision=(terminal_width-size)/2
+	if [[ $provision -lt 0 ]]; then
+		provision=0
+	fi
+	tput cuf $provision
+	printf "$1"
+}
+
+echo -e "\n" #Put new line
+
+printf "\x1b[41;97;1m" #41=red BG; 97=bright white FG; 1=bold
+put_line " FBI WARNING "
+printf "\x1b[0;97m\n"
+
+put_line "Federal Law provides severe civil and criminal penalties" ; echo ""
+put_line "for the unauthorized reproduction, distribution, or exhibition" ; echo ""
+put_line "of copyrighted motion pictures (Title 17, United States Code," ; echo ""
+put_line "Sections 501 and 508). The Federal Bureau of Investigation " ; echo ""
+put_line "investigates allegations of criminal copyright infringement" ; echo ""
+put_line "(Title 17, United States Code, Section 506)." ; echo ""
+
+echo -e "\x1b[0m" #Reset all attributes, put new line
+
+let remaining=$terminal_height-11
+for (( i=0; i<$remaining; i++)); do
+	echo ""
+done
+
+tput cup 11 0
